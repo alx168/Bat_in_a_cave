36f4e19e92bac5eb345c5622d33d0f5eda618dbd
alx168 <alx168@gmail.com>
2024-11-24 22:32:16 -0800

    magnitude of flight bar complete. TODO: Direction bar.

---
+++ flight_power_bar.gd
@@ -0,0 +1,25 @@
+extends ProgressBar
+
+@onready var timer = $FlightTimer
+@onready var flight_charge_bar = $FlightCharge
+
+var flight_charge = 0 : set = _set_charge
+
+func _set_charge(new_charge):
+	var prev_charge = flight_charge
+	value = new_charge 
+	
+	#if charge >= 0 and charge < 100:
+		#flight_charge_bar.value = 0
+	#if charge >= 100:
+		#flight_charge_bar.value = 100 
+
+#func init_flight_charge(_charge):
+	#charge = 0
+	#max_value = 100.0
+	#min_value = 0
+	#value = charge
+	#flight_charge_bar.max_value = 100.0
+	#flight_charge_bar.min_value = 0
+	#flight_charge_bar.value = charge
+	#print("init values: charge: " + str(charge) + " min: " + str(min_value) + " value: " + str(value) + " max_value: " + str(max_value))
--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 ### UI ###
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
+@onready var flight_charge_bar = $FlightBar
 
 ### INPUTS ###
 var input_flap: bool = false
@@ -13,16 +14,31 @@ const flight_acceleration: float = -2
 const gravity: float = .1
 const terminal_fall_speed: float = 5
 
+### GAMEPLAY ###
+const max_flight_charge: float = 100
+var flight_charge: float = 0
+const flight_charge_step: float = 0.1
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	#flight_charge_bar.init_flight_charge(max_flight_charge)
 
 # handle inputs and visuals
 func _process(delta: float) -> void:
+	#print(flight_charge)
 	if Input.is_action_just_pressed("button_press"):
 		input_flap = true
+	if Input.is_action_pressed("button_press") and flight_charge < max_flight_charge:
+		#testing flight values
+		flight_charge = flight_charge + flight_charge_step
+	#uncomment below if we want a decreasing bar effect
+	#elif not Input.is_action_pressed("button_press") and flight_charge >= 0:
+		#flight_charge = flight_charge - flight_charge_step
+	_set_flight_charge(flight_charge)
 	
 func _physics_process(delta: float) -> void:
+	print(flight_charge)
 	if input_flap:
 		grounded = false
 		velocity += Vector2(0, flight_acceleration)
@@ -36,5 +52,6 @@ func _physics_process(delta: float) -> void:
 	if collisionInfo != null:
 			var grounded: bool = true
 			velocity = Vector2.ZERO
-	
-	
+
+func _set_flight_charge(value):
+	flight_charge_bar.flight_charge = flight_charge
