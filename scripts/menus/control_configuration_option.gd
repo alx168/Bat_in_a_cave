18a8b1d0b5b270e1ebb5cb7e30f74dd6b95b9b97
Alexander Bieniek <BieniekAlexander@gmail.com>
2024-11-28 00:17:25 -0600

    finishing implementation of button configuration

--- scripts/menus/control_configuration_option.gd
+++ scripts/menus/control_configuration_option.gd
@@ -5,16 +5,20 @@ extends MenuOption
 # two variables that tell us if the user 
 var is_button_freed: bool
 var is_configuring: bool
+var field: Variant
+var regex = RegEx.new()
 
 func _ready() -> void:
 	is_button_freed = false
+	field = get_node('.')
+	regex.compile(r"\(.+\)")
 	
 func _input(event):
 	if is_button_freed and (event is InputEventKey):
-		print("hello")
 		InputMap.action_add_event(InputManager.INPUT_EVENT_NAME, event)
 		is_button_freed = false
 		is_configuring = false
+		field.text = regex.sub(field.text, "(%s)" % event.as_text())
 
 func _process_option_held(delta: float, held: bool) -> void:
 	if held:
