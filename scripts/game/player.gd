b140d67003788172b1591beafe81188887da5d24
Alexander Bieniek <BieniekAlexander@gmail.com>
2024-11-28 02:25:38 -0600

    adding controls to make turning during flight make a bit more sense
    now, your lift rotation angle resets if you haven't been flapping for bit

--- scripts/game/player.gd
+++ scripts/game/player.gd
@@ -4,61 +4,82 @@ extends CharacterBody2D
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 @onready var flight_charge_bar = $FlightBar
+@onready var aim_indicator: Line2D = $AimIndicator
 
 ### INPUTS ###
-var input_flap: bool = false
-var input_charge: bool = false
+var tapped: bool = false
+var held: bool = false
 
 ### PHYSICS ### https://www.youtube.com/watch?v=p3jDJ9FtTyM&t=58s
-var grounded: bool = false
-const flight_acceleration: float = -2
-const gravity: float = .1
-const terminal_fall_speed: float = 10
+var is_grounded: bool = false
+const GRAVITY: float = .1
+const TERMINAL_FALL_SPEED: float = 7.5
+const FLAP_ROTATION_BUFFER_THRESHOLD: float = .3
+var flap_rotation_buffer: float = FLAP_ROTATION_BUFFER_THRESHOLD # how long we'll keep rotating with flaps before changing direction
+var flight_rotation_degrees: float = 30
 
 ### GAMEPLAY ###
 const max_flight_charge: float = 100
 var flight_charge: float = 0
 const flight_charge_step: float = 0.1
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	#flight_charge_bar.init_flight_charge(max_flight_charge)
 
 # handle inputs and visuals
-func _process(delta: float) -> void:
+func _collect_inputs(delta:float) -> void:
 	var input_type: InputManager.InputType = InputManager.poll_input(delta)
 	
 	if input_type == InputManager.InputType.TAPPED:
-		input_flap = true
+		tapped = true
 	elif input_type == InputManager.InputType.HELD:
-		input_charge
-	
+		held = true
+		
+func _update_hud() -> void:
+	aim_indicator.global_position = position+velocity*20
+
+func _process(delta: float) -> void:
+	_collect_inputs(delta)
+	_update_hud()
+
+# handle state and physics
 func _physics_process(delta: float) -> void:
-	if input_flap:
-		grounded = false
-		var rotation_degrees: float = 30 if velocity.x<0 else -30
-		velocity = velocity.rotated(deg_to_rad(rotation_degrees))
-		# Vector2(0, flight_acceleration)
-		input_flap = false
-	elif not grounded:
-		velocity += Vector2(0, gravity)
+	if is_grounded:
+		if tapped:
+			# TODO mirror aim rotation
+			pass
+		elif held:
+			print(flight_charge)
+			flight_charge = flight_charge + flight_charge_step
+			_set_flight_charge(flight_charge)
+		#uncomment below if we want a decreasing bar effect
+		#else:
+			#flight_charge = flight_charge - flight_charge_step
+	else: # not is_grounded
+		velocity += Vector2(0, GRAVITY)
 		
-	if input_charge:
-		print(flight_charge)
-		flight_charge = flight_charge + flight_charge_step
-		_set_flight_charge(flight_charge)
-	#uncomment below if we want a decreasing bar effect
-	#else:
-		#flight_charge = flight_charge - flight_charge_step
+		if tapped:
+			if flap_rotation_buffer >= FLAP_ROTATION_BUFFER_THRESHOLD:
+				print("flipping")
+				flight_rotation_degrees = 30 if velocity.x<0 else -30
+			velocity = velocity.rotated(deg_to_rad(flight_rotation_degrees))
+			tapped = false
+			flap_rotation_buffer = 0.0
+		else:
+			flap_rotation_buffer += delta
+			 
+		if held:
+			# TODO flap to slow down?
+			pass
+			
+			
+		velocity = Vector2(velocity.x, clamp(velocity.y, -2*TERMINAL_FALL_SPEED, TERMINAL_FALL_SPEED))	
 		
-	velocity = Vector2(velocity.x, clamp(velocity.y, -2*terminal_fall_speed, terminal_fall_speed))	
-	var collisionInfo: KinematicCollision2D = move_and_collide(velocity)
-	
-	if collisionInfo != null:
-			var grounded: bool = true
+		if move_and_collide(velocity) != null:
+			var is_grounded: bool = true
 			velocity = Vector2.ZERO
+
 
 func _set_flight_charge(value):
 	flight_charge_bar.flight_charge = flight_charge
