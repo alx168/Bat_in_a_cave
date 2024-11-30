e9d31b4637d28e2e644d3197df7f992fe8c45489
Alexander Bieniek <BieniekAlexander@gmail.com>
2024-11-29 20:51:48 -0600

    adding a fog of war to scene for starters

---
+++ scripts/game/fog_layer.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+
+var display_width = ProjectSettings.get("display/window/size/viewport_width")
+var display_height = ProjectSettings.get("display/window/size/viewport_height")
+
+var fogImage = Image.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
